e6c87fb0a7ae04617cb9cd72ceb24de828f66afc
Christian Hermann <christian.hermann94@alice.degit config --global user.name Christian>
2026-06-19 22:02:57 +0200

    deleted unneeded files

--- lib/features/import/import_provider.dart
+++
@@ -1,3 +0,0 @@
-// Workbook import state management
-// Riverpod providers for import workflow
-// TODO: Implement providers for file parsing and data import (not MVP priority)
--- lib/features/import/import_screen.dart
+++
@@ -1,3 +0,0 @@
-// Workbook import screen
-// Import training data from Excel file
-// TODO: Implement UI for file selection and import (not MVP priority)
